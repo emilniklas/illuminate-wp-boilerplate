7741e86612aac866a6ccc9c10d4b0f8a1b4feeb8
Emil Persson <emil.persson@creuna.se>
2016-08-24 18:51:22 +0200

    Do a little inline templating

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,5 +2,16 @@
 
 $router->get('/', function (App\PostRepository $posts) {
     $post = $posts->find(1);
-    return json_encode($post);
+    return "
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>{$post->title}</title>
+            </head>
+            <body>
+                <h1>{$post->title}</h1>
+                {$post->body}
+            </body>
+        </html>
+    ";
 });
